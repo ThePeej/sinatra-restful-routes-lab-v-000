8baeab7b78be19b99df79f3a92082f248542e687
PJ Frias <peejfrias@gmail.com>
2018-02-06 15:11:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   end
   get '/' do
     "<h1>Sintra RESTful Routes Lab</h1>"
+    "<h3>Navigate to '/recipes' to get started"
   end
 
   get '/recipes' do
