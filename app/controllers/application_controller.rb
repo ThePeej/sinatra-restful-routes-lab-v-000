cf67ab3811ce67ad30c2b02f78cf30dfcc0e08a2
PJ Frias <peejfrias@gmail.com>
2018-02-06 15:38:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch 'recipes/:id' do
+    raise params.inspect
     @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
